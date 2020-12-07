8368d8f9892c6609886415b58c6ae6a64308c20f
Antonin Cezard <acezard@gmail.com>
2020-12-07 16:50:55 +0100

    fix: update config and make working build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://nice-island-01456b003.azurestaticapps.net'
+  },
   plugins: [
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "G-P7STY5MN05",
       },
     },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    "gatsby-plugin-manifest",
-    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
